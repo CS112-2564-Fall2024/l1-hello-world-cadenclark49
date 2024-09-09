2f9445d8858beee36888caa8ed5fa973ffc64625
cadenclark49 <cadenclark49@gmail.com>
2024-09-09 19:58:51 +0000

    Lab 01 Finished

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+//Ai was used to aid in this project
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!");
+
+        String randomPrompt = "Your random number is: ";
+
+        randomNum();
+        promptRandom(randomPrompt);
+    }
+
+    public static void promptRandom(String randomPrompt) {
+
+        System.out.println(randomPrompt + randomNum());
+    }
+    
+
+    public static double randomNum() {
+        return Math.round(Math.random() * 10);
+        
     }
 }
